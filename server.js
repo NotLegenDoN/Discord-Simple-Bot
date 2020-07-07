2faa2144cd64c057ee2a681b120033b72507cb65
NotLegenDoN <65687804+NotLegenDoN@users.noreply.github.com>
2020-07-07 18:33:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,26 +7,26 @@ client.on('ready', () => {
   console.log(`LISTO`);
 });
 
-client.on('message', messagge => {
+client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
 });
 
-client.on("message", message =>{
+client.on("message", message => {
   var PREFIX = "!";
-  if(message.content.startsWith(PREFIX+"say")){
-    const args = message.content.split(".");
-    let canal = message.mentions.channels.first();
-    if(!args[1]){
-      return message.channel.send("¿Qué mensaje quieres que envie?");
+    if(message.content.startsWith(PREFIX+"say")){
+      const args = message.content.split(".");
+      let canal = message.mentions.channels.first();
+      if(!args[1]){
+        return message.channel.send("¿Qué mensaje quieres que envie?");
+      }
+      if(!canal){
+        return message.channel.send("¿A qué canal de texto quieres que envie el mensaje?");
+      }
+      message.channel.send("Mensaje enviado...").then(message => message.delete({timeout: 5000}));
+      canal.send(args[1]);
     }
-    if(!canal){
-      return message.channel.send("¿A qué canal de texto quieres que envie el mensaje?");
-    }
-    message.channel.send("Mensaje enviado...").then(message => message.delete({timeout: 5000}));
-    canal.send(args[1]);
-  }
-})
+  });
 
 client.login('NzMwMTAyNDUyNjU2OTMwOTg3.XwSnUw.PhXOXQWzpaUVv8xME_VeKVzRwfk');
