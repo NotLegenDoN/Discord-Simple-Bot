11909caa8b383b622ce25c1a5e8ee0b3bbfab5c1
NotLegenDoN <65687804+NotLegenDoN@users.noreply.github.com>
2020-07-07 18:31:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
 
 //////////bot real/////////////
-const { Client, MessageEmbed  } = require("discord.js");
+const { Client, MessageEmbed, Message  } = require("discord.js");
 const client = new Client();
 
 client.on('ready', () => {
   console.log(`LISTO`);
 });
 
-client.on('message', msg => {
+client.on('message', messagge => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
-    var PREFIX = "!";
-      if(message.content.startsWith(PREFIX+"say")){
-        const args = message.content.split(".");
-        let canal = message.mentions.channels.first();
-        if(!args[1]){
-          return message.channel.send("¿Qué mensaje quieres que envie?");
-        }
-        if(!canal){
-          return message.channel.send("¿A qué canal de texto quieres que envie el mensaje?");
-        }
-        message.channel.send("Mensaje enviado...").then(message => message.delete({timeout: 5000}));
-        canal.send(args[1]);
-      }
 });
+
+client.on("message", message =>{
+  var PREFIX = "!";
+  if(message.content.startsWith(PREFIX+"say")){
+    const args = message.content.split(".");
+    let canal = message.mentions.channels.first();
+    if(!args[1]){
+      return message.channel.send("¿Qué mensaje quieres que envie?");
+    }
+    if(!canal){
+      return message.channel.send("¿A qué canal de texto quieres que envie el mensaje?");
+    }
+    message.channel.send("Mensaje enviado...").then(message => message.delete({timeout: 5000}));
+    canal.send(args[1]);
+  }
+})
 
 client.login('NzMwMTAyNDUyNjU2OTMwOTg3.XwSnUw.PhXOXQWzpaUVv8xME_VeKVzRwfk');
