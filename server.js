361de60bbf9ab49fc7fbf15da31b00083d0e4042
NotLegenDoN <65687804+NotLegenDoN@users.noreply.github.com>
2020-07-07 18:27:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,3 @@
-const http = require('http');
-const express = require('express');
-const app = express();
-
-//
-app.use(express.static('public'));
-
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-app.get("/", (request, response) => {
-  response.sendStatus(200);
-});
-
-app.listen(process.env.PORT);
-
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`); 
-}, 280000);
 
 //////////bot real/////////////
 const Discord = require('discord.js');
