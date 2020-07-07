a37d43a95d87995d4b0c2931cd18ba38ccc77638
NotLegenDoN <65687804+NotLegenDoN@users.noreply.github.com>
2020-07-07 18:30:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 //////////bot real/////////////
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, MessageEmbed  } = require("discord.js");
+const client = new Client();
 
 client.on('ready', () => {
   console.log(`LISTO`);
