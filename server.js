57f80a60def4c1f43fb55a4b4bc86ef916ccd181
NotLegenDoN <65687804+NotLegenDoN@users.noreply.github.com>
2020-07-07 18:24:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,19 @@ client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
+    var PREFIX = "!";
+      if(message.content.startsWith(PREFIX+"say")){
+        const args = message.content.split(",");
+        let canal = message.mentions.channels.first();
+        if(!args[1]){
+          return message.channel.send("¿Qué mensaje quieres que envie?");
+        }
+        if(!canal){
+          return message.channel.send("¿A qué canal de texto quieres que envie el mensaje?");
+        }
+        message.channel.send("Mensaje enviado...").then(message => message.delete({timeout: 5000}));
+        canal.send(args[1]);
+      }
 });
 
 client.login('NzMwMTAyNDUyNjU2OTMwOTg3.XwSnUw.PhXOXQWzpaUVv8xME_VeKVzRwfk');
